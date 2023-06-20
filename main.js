84a8a1ed7b4c0592a51ada6a1ad9af9244ec37e3
sirhmvfx <sirhmvfx@gmail.com>
2023-06-20 16:08:51 +0100

    hsyhhhhsysy

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ const menu = document.querySelector(".navbar");
 hamburger.addEventListener("click", function () {
   menu.classList.toggle("is-active");
 });
+
+const carousel = document.querySelector(".review-container");
+const arrowBtn = document.querySelectorAll(".review button");
+const firstCardWidth = carousel.querySelector(".review-cards").offsetWidth;
+
+arrowBtn.forEach(function (btn) {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft +=
+      btn.id === "left" ? -firstCardWidth : +firstCardWidth;
+  });
+});
